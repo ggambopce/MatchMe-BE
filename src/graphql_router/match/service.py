8d7d22059e1522bd7e020a3d4308bfc7bb4c5e9c
ggambopce <ggambopce@gmail.com>
2025-06-28 21:24:06 +0900

    feat: 매치 스코어와 매치 리포트 생성 메섲드 작성

--- src/graphql_router/match/service.py
+++ src/graphql_router/match/service.py
@@ -1,7 +1,10 @@
 # graphql_router/match/service.py
-
-from typing import List
+from graphql_router.profile.types import MatchProfileResponse
 from graphql_router.match.types import MatchResultType
+from graphql_router.match.enum import TEMPERAMENT_PAIR_REPORT_MAP
+from graphql_router.profile.enum import TEMPERAMENT_MAP, ENNEAGRAM_MAP
+from graphql_router.profile.types import UserChoiceInput
+
 
 class MatchService:
     
@@ -18,17 +21,78 @@ class MatchService:
         # 예시 반환
         return None
 
-    # 관리자용 결과 내용 조회
+
+    # 매치 스코어와 매치리포트 생성 메서드
     @staticmethod
-    async def get_my_match_result(user_id: str) -> MatchResultType:
-        """
-        주어진 user_id를 기준으로, 관련된 match_result들을 조회하여
-        MatchResultType 리스트로 반환한다.
-        """
-        # TODO: user_id로 profile 조회
-        # TODO: profile1_id 또는 profile2_id에 해당하는 match_result 검색
-        # TODO: 각 결과를 MatchResultType으로 매핑
+    async def set_match_score_match_report(
+        user1_choices: list[UserChoiceInput],
+        user2_choices: list[UserChoiceInput]
+        ) -> MatchProfileResponse:
+        # 선택값 딕셔너리로 정리
+        u1 = {c.question_number: c.choice_number for c in user1_choices}
+        u2 = {c.question_number: c.choice_number for c in user2_choices}
+        
+        score = 0
 
-        # 예시 반환
-        return None
+        '''Temperament 기질(Q1)
+        25점인 경우
+        1번-1번, 2번-2번, 3번-4번, 4번-3번
+        나머지 쌍은 20점
+        '''
+        t1 = TEMPERAMENT_MAP.get(u1[1])
+        t2 = TEMPERAMENT_MAP.get(u2[1])
+        if t1 == t2 or (u1[1], u2[1]) in [(3, 4), (4, 3)]:
+            score += 25
+        else:
+            score += 20
 
+        match_report = TEMPERAMENT_PAIR_REPORT_MAP.get((t1, t2)) or TEMPERAMENT_PAIR_REPORT_MAP.get((t2, t1)) or "기질 궁합 리포트 없음"
+
+        ''' Enneagram 애니어그램(Q2)
+        25점인 경우 - 합쳐서 10인 경우 
+        1번-9번/2번-8번/...5번-5번/...../9번-1번
+        나머지 쌍은 20점
+        '''
+        e1 = u1[2]
+        e2 = u2[2]
+        score += 25 if (e1 + e2 == 10) else 20
+
+        ''' Maturity 성숙도 (Q3)
+        25점인 경우
+        1-1/2-2/3-3 같은 수준끼리 만났을 때.
+        20점인 경우
+        1-2/2-3/2-1/3-2 한 단계씩 차이나게 만났을 때
+        15점인 경우 
+        1-3/3-1 두 단계씩 차이나게 만났을 때
+        '''
+        m1 = u1[3]
+        m2 = u2[3]
+        diff = abs(m1 - m2)
+        if diff == 0:
+            score += 25
+        elif diff == 1:
+            score += 20
+        else:
+            score += 15
+
+        '''Instinct 본능 (Q4)
+        25점인 경우 
+        1-1/2-2/3-3 같게 만났을 때
+        20점인 경우
+        1-2/1-3/2-1/3-1만났을 때
+        15점인 경우
+        나머지
+        '''
+        i1 = u1[4]
+        i2 = u2[4]
+        if i1 == i2:
+            score += 25
+        elif (i1 == 1 and i2 in [2, 3]) or (i2 == 1 and i1 in [2, 3]):
+            score += 20
+        else:
+            score += 15
+
+        return MatchProfileResponse(
+            match_score=score,
+            match_report=match_report
+        )
---
+++ src/graphql_router/profile/service.py
@@ -0,0 +1,31 @@
+from graphql_router.profile.types import ProfileType
+from graphql_router.profile.enum import TEMPERAMENT_MAP, ENNEAGRAM_MAP, TEMPERAMENT_REPORT_MAP
+
+class ProfileService:
+     
+    # 설문지 입력값 기반 가공 후 변수에 저장 
+    # 1번에서 temperament 결정
+    # 2번에서 에니어그램 유형 결정
+    # 프로필 작성을 위해서 우선 2개만 결정
+    @staticmethod
+    async def setMyPofile(user_choice_list: list[dict]) -> ProfileType:
+        temperament = None
+        enneagram = None
+
+        # 설문 응답 순회하며 필요한 항목 추출
+        for choice in user_choice_list:
+            qn = choice["question_number"]
+            cn = choice["choice_number"]
+
+            if qn == 1:  # 기질 결정
+                temperament = TEMPERAMENT_MAP.get(cn)
+                temperament_report = TEMPERAMENT_REPORT_MAP.get(temperament)
+            elif qn == 2:  # 에니어그램 결정
+                enneagram = ENNEAGRAM_MAP.get(cn)
+
+        # 결과를 ProfileType에 매핑
+        return ProfileType(
+            temperament=temperament,
+            enneagram=enneagram,
+            temperament_report=temperament_report
+        )
